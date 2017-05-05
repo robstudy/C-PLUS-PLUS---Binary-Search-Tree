40f406a128e58a73f50aa259ba9b0fb8b35ba63c
Robert Garza <robstudent1988@gmail.com>
2017-05-04 23:09:57 -0700

    Implemented insert and print functions with helper functions to call recursively. BinaryTree<int> bs in main for testing

--- BinaryTree.h
+++ BinaryTree.h
@@ -4,17 +4,70 @@ using namespace std;
 template <class T>
 class BinaryTree {
 	public:
+		BinaryTree();
 		void insert(T);
 		void remove(T);
+		void print();
 		bool search(T);
 
 	private:
 		struct Branch {
 			Branch *leftLeaf;
-			Branch *rightLeft;
-			Branch *parent;
+			Branch *rightLeaf;
 			T data;
+			Branch(Branch *l = NULL, Branch *r = NULL, T d = NULL):
+				leftLeaf(l),
+				rightLeaf(r),
+				data(d){}
 		};
 
 		Branch *root;
+		void b_insert(Branch *&, T);
+		void b_print(Branch *);
+		unsigned int size;
+};
+
+template <class T>
+BinaryTree<T>::BinaryTree() {
+	size = 0;
+};
+
+template <class T>
+void BinaryTree<T>::insert(T data) {
+	if(!root) {
+		root = new Branch(NULL, NULL, data);
+		size++;
+		return;
+	}
+
+	if(root->data == data) return;
+	else if (root->data > data) b_insert(root->leftLeaf, data);
+	else b_insert(root->rightLeaf, data);
+};
+
+template <class T>
+void BinaryTree<T>::b_insert(Branch *&leaf, T data) {
+	if (!leaf) {
+		leaf = new Branch(NULL, NULL, data);
+		size++;
+	}
+
+	if(leaf->data == data) return;
+	else if (leaf->data > data) b_insert(leaf->leftLeaf, data);
+	else b_insert(leaf->rightLeaf, data);
+};
+
+template <class T>
+void BinaryTree<T>::print() {
+	if(!root) return;
+	b_print(root);
+};
+
+template <class T>
+void BinaryTree<T>::b_print(Branch *leaf) {
+	if(!leaf) return;
+	
+	cout << leaf->data << endl;
+	b_print(leaf->leftLeaf);
+	b_print(leaf->rightLeaf);
 };
--- main.cc
+++ main.cc
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main() {
-
+	BinaryTree<int> bt;
+	bt.insert(10);
+	bt.insert(4);
+	bt.insert(20);
+	bt.insert(5);
+	bt.insert(15);
+	bt.insert(3);
+	bt.insert(7);
+	bt.insert(13);
+	bt.print();
 	return 0;
 }
